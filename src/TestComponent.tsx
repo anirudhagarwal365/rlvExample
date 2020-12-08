a27560daa85be1e90584441df5598d87890e2ed0
Anirudh Agarwal <anirudh.agarwal@flipkart.com>
2020-12-08 22:26:10 +0530

    Replicated issue for first index

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -10,6 +10,53 @@ interface TestComponentState {
     visibleIndex: number;
 }
 
+const actualHeights = {
+    "0": 241.42857360839844,
+    "1": 233.42857360839844,
+    "2": 421.1428527832031,
+    "3": 233.42857360839844,
+    "4": 139.7142791748047,
+    "5": 139.7142791748047,
+    "6": 233.42857360839844,
+    "7": 233.42857360839844,
+    "8": 139.7142791748047,
+    "9": 233.42857360839844,
+    "10": 233.42857360839844,
+    "11": 327.4285583496094,
+    "12": 233.42857360839844,
+    "13": 139.7142791748047,
+    "14": 421.1428527832031,
+    "15": 139.7142791748047,
+    "16": 233.42857360839844
+    };
+
+    const staticHeights = {
+        "0": 231.71428571428572,
+        "1": 223.71428571428572,
+        "2": 407.42857142857144,
+        "3": 223.71428571428572,
+        "4": 131.85714285714286,
+        "5": 131.85714285714286,
+        "6": 223.71428571428572,
+        "7": 223.71428571428572,
+        "8": 131.85714285714286,
+        "9": 223.71428571428572,
+        "10": 223.71428571428572,
+        "11": 315.57142857142856,
+        "12": 223.71428571428572,
+        "13": 131.85714285714286,
+        "14": 407.42857142857144,
+        "15": 131.85714285714286,
+        "16": 223.71428571428572,
+        "17": 223.71428571428572,
+        "18": 131.85714285714286,
+        "19": 131.85714285714286,
+        "20": 223.71428571428572,
+        "21": 131.85714285714286,
+        "22": 131.85714285714286,
+        "23": 131.85714285714286
+    };
+
 export default class TestComponent extends React.Component<any, TestComponentState> {
     dataProvider: DataProvider;
 
@@ -29,7 +76,8 @@ export default class TestComponent extends React.Component<any, TestComponentSta
     };
 
     private _getLayoutHeight(index: number) {
-        return 100;
+        // return this.getRandom() * 10;
+        return staticHeights[index];
     }
 
     private onScroll = (_rawEvent: ScrollEvent, _offsetX: number, _offsetY: number) => {
@@ -52,11 +100,11 @@ export default class TestComponent extends React.Component<any, TestComponentSta
 
     private rowRenderer = (_: React.ReactText, section: any, index: number): JSX.Element | null => {
         return (
-            <View>
+            <View style={{backgroundColor: 'teal', height: actualHeights[index]}}>
                 <View style={{padding: 16, backgroundColor: 'green'}}>
-                    <Text> Header </Text>
+                    <Text>{index}</Text>
                 </View>
-                {this.getBody(index)}
+                {/* {this.getBody(index)} */}
             </View>
         );
     };
@@ -88,7 +136,7 @@ export default class TestComponent extends React.Component<any, TestComponentSta
     render() {
         return (
             <View style={{height: 500, flexDirection: 'row'}}>
-                <View style={{flex: 9}}>
+                <View style={{flex: 9, borderColor: 'blue', borderWidth: 2}}>
                     <View style={{height: 100, backgroundColor: 'blue'}} />
                     <RecyclerListView
                         ref={this._setProgressiveListViewRef}
